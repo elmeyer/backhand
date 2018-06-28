08b7ea31a0bad7ba104f3b2b80ede0f279a4094f
Lars Meyer <lars.meyer@uni-weimar.de>
2018-06-28 09:55:26 +0200

    Fix structure; this is just a module now

--- backhand/src/androidTest/java/com/elmeyer/backhand/ExampleInstrumentedTest.java
+++ backhand/src/androidTest/java/com/elmeyer/backhand/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.elmeyer.backhand", appContext.getPackageName());
+        assertEquals("com.elmeyer.backhand.test", appContext.getPackageName());
     }
 }
